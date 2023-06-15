b29bcaabef03b340a2e4e5eb20a6b39c9ba3f32f
David <davidjayfsu@gmail.com>
2023-06-15 18:18:12 -0400

    Started on serial2arduino node

---
+++ serial2arduino/serial2arduino/testcontroller.py
@@ -0,0 +1,33 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+class testcontroller(Node):
+
+    def __init__(self):
+        #This is what the node will be named according to ros:
+        super().__init__('testcontroller')
+        self.publisher_ = self.create_publisher(String,'stuff',10)
+        timer_period = 1
+        self.timer = self.create_timer(timer_period,self.timer_callback)
+
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World'
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: %s' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    ctrl = testcontroller()
+
+    rclpy.spin(ctrl)
+
+    ctrl.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()

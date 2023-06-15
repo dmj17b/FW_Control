cacd32ec92fbe349dc3105c834f4ba7150f5ba01
David <davidjayfsu@gmail.com>
2023-06-15 18:16:42 -0400

    Started on a serial2arduino node for sending commands to arduino

--- Docker/BuildAndRun_Windows.bat
+++ Docker/BuildAndRun_Windows.bat
@@ -1,4 +1,4 @@
 
 docker build -t airplanecontrol .
 
-docker run --rm --net host -it --mount type=bind,source=".",target=/FW_Control -it airplanecontrol
+docker run --rm --net host -it --mount type=bind,source="C:\Users\david\OneDrive - Florida State University\TestROS\FW_Control",target=/FW_Control/src -it airplanecontrol
